bce0788d9688354cd7ae5f34c25d1226ea585638
Nthabiseng Mashalane <nthabimashalane0@gmail.com>
2024-11-18 17:15:19 +0200

    Committed Changes

--- api.js
+++ api.js
@@ -10,13 +10,7 @@ import {
     documentId
 } from "firebase/firestore/lite"
 
-const firebaseConfig = {
-    authDomain: "vanlife-a1af5.firebaseapp.com",
-    projectId: "vanlife-a1af5",
-    storageBucket: "vanlife-a1af5.appspot.com",
-    messagingSenderId: "803007000356",
-    appId: "1:803007000356:web:446cd3a1ca406839258db1"
-};
+
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
